4b79cc3a06ae0f26681e798f7e3b839d9e3628a7
lamkhanghuynh09 <khanghuynh203104@gmail.com>
2025-05-28 20:45:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -46,5 +46,8 @@ def login():
     else:
         return "Sai tài khoản hoặc mật khẩu", 401
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=81)
+    port = int(os.environ.get("PORT", 5000))  # Render cung cấp PORT qua biến môi trường
+    app.run(host='0.0.0.0', port=port)
